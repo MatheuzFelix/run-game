cf9fc79adcd629b3d8cf02a98f2d25834024a9f7
MatheuzFelix <matheuzfelixamorim@gmail.com>
2022-10-15 21:59:57 -0300

    atualizando o game

--- js/script.js
+++ js/script.js
@@ -16,15 +16,20 @@ const loop = setInterval(() => {
     const pipeposition = pipe.offsetLeft;
     const runposition =  +window.getComputedStyle(run).bottom.replace('px','');
 
-    console.log(runposition )
-    console.log(runposition)
 
-    if (pipeposition <= 120) {
+    if (pipeposition <= 120 && pipeposition > 0 && runposition < 80) {
 
         pipe.style.animation = 'none';
-        pipe.style.left = `${pipeposition}px`;
-        pipe.style
+        pipe.style.left = `${120}px`;
 
+        run.style.animation = 'none';
+        run .style.bottom = `${0}`;
+
+        run.src = './Imagens/over.gif'
+        run.style.width = '75px'
+        run.style.marginleft = '50px'
+
+        clearInterval(loop);
 
     }
 
